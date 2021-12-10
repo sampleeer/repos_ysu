d48271cc073dc764a15101ae406d1be5537e59a5
Mahmud rajjanian Abdullah mammegov <pavel.ushlepkov@yandex.ru>
2021-12-10 23:17:28 +0300

    add functions.h and main.cpp and functions.cpp

---
+++ functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Функция x^4  + 5 * x^2 + 3
+void changed_matrix(int** matrix, int n);
+
+//создание матрицы 
+int** create(int n);
+
+//проверка размерности матрицы
+int check_dimension();
+
+//проверка вводимых значений матрицы
+int check_element();
+#endif //FUNCTIONS_H
