32a4a9cf58fe4af877e1847c8f62fb07fb4c0f4b
Mahmud rajjanian Abdullah mammegov <86608170+sampleeer@users.noreply.github.com>
2022-02-03 15:01:49 +0300

    лабораторная 6.2

--- matrix.cpp
+++ matrix.cpp
@@ -17,13 +17,14 @@ void correct_input_N(int& num) {
 //проверка введеного действительного числа
 void correct_input_R(double& num) {
 	cin >> num;
-	while (cin.fail() ) {
+	while (cin.fail()) {
 		cin.clear();
 		cin.ignore(32767, '\n');
 		cout << "Введите вещественное значение переменной!\n";
 		cin >> num;
 	}
 }
+
 
 //создание нулевой квадратной матрицы
 double** matrix_nul_create(int n) {
@@ -61,57 +62,52 @@ double** matrix_input(int n) {
 void matrix_output(double** orig_matr, int n) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++)
-			cout << orig_matr[i][j];
+			cout << setw(10) << fixed << setprecision(0) << orig_matr[i][j];
 		cout << endl;
 	}
 }
 
-//Возвращает матрицу для нахождения минора, путем вырчеркивания row-ой строки и col-того столбца матрицы matr;
-void matrix_for_minor(double** matr, int n, int row, int col, double** minor_matr) {
-	int row_shift = 0; //Смещение индекса строки в матрице
-	int col_shift = 0; //Смещение индекса столбца в матрице
-	for (int i = 0; i < n - 1; i++) {
-		//Пропустить row-ую строку
-		if (i == row)
-			row_shift = 1;
-		for (int j = 0; j < n - 1; j++) {
-			//Пропустить col-ый столбец
-			if (j == col)
-				col_shift = 1;
-			minor_matr[i][j] = matr[i + row_shift][j + col_shift];
+//функция сложения квадратной матрицы с числом
+double** matrix_addition_num(double** matr, int n, int num) {
+	double** out_matr = matrix_nul_create(n);
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++) {
+			if (i == j)
+				out_matr[i][j] = num + matr[i][j];
+			else
+				out_matr[i][j] = matr[i][j];
 		}
-	}
+	return out_matr;
 }
 
-//Рекурсивная функция для нахождения определителя матрицы. 
-int matrix_det(double** matr, int n) {
-	//временная матрица, полученная вычеркиванием i-ой строки и 0-ого столбца
-	double** temp = matrix_nul_create(n - 1);
-	double det = 0;
-	double alg_dop = 0;
-	//если дошли до минора 2х2, то возвращаекм его
-	if (n == 2) {
-		alg_dop = matr[0][0] * matr[1][1] - matr[0][1] * matr[1][0];
-		return alg_dop;
-	}
-	//условие выхода из рекурсии
-	if (n == 1) {
-		return matr[0][0];
-	}
-	//знак для высчитывания алгебраического дополнения
-	int sign = 1;
-	for (int i = 0; i < n; i++) {
-		//Удаляем из матрицы i-ую строку и 0-ый столбец
-		matrix_for_minor(matr, n, i, 0, temp);
+//функция умножения квадратной матрицы на число
+double** matrix_multiply_num(double** matr, int n, int num) {
+	double** out_matr = matrix_nul_create(n);
 
-		double det_minor = matrix_det(temp, n - 1);
-		alg_dop = (sign * matr[i][0] * det_minor);
-
-		//меняем знак
-		sign *= -1;
-		det += alg_dop;
-	}
-	matrix_memory_clear(temp, n - 1);
-	return det;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			out_matr[i][j] = num * matr[i][j];
+	return out_matr;
 }
 
+//функция сложения квадратных матриц
+double** matrix_addition_matrix(double** matr_1, double** matr_2, int n) {
+	double** out_matr = matrix_nul_create(n);
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			out_matr[i][j] = matr_1[i][j] + matr_2[i][j];
+	return out_matr;
+}
+
+//функция перемножения квадратных матриц
+double** matrix_multiply_matrix(double** matr_1, double** matr_2, int n) {
+	double** out_matr = matrix_nul_create(n);
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int k = 0; k < n; k++)
+				out_matr[i][j] += matr_1[i][k] * matr_2[k][j];
+	return out_matr;
+}
--- matrix.h
+++ matrix.h
@@ -1,29 +1,16 @@
 #ifndef MATRIX_OPERATIONS
 #define MATRIX_OPERATIONS
 
-//проверка введеного натурального числа
-void correct_input_N(int& num);
+double** minor(double* matrix[], int size, int deleted_row, int deleted_column);
 
-//проверка введеного действительного числа
-void correct_input_R(double& num);
+double matrix_determinant(double* matrix[], int size);
 
-//создание нулевой квадратной матрицы
-double** matrix_nul_create(int n);
+double** inverse_matrix(double* matrix[], int size);
 
-//отчистка памяти после создания матрицы
-void matrix_memory_clear(double** matr, int n);
+void clear(double* arr[], int size);
 
-//ввод начальной квадратной матрицы
-double** matrix_input(int n);
+void mult(double* mat1[], double* mat2[], int size);
 
-//вывод начальной квадратной матрицы
-void matrix_output(double** orig_matr, int n);
-
-//Возвращает матрицу для нахождения минора, путем вырчеркивания row-ой строки и col-того столбца матрицы matr;
-void matrix_for_minor(double** matr, int n, int row, int col, double** minor_matr);
-
-//Рекурсивная функция для нахождения определителя матрицы. 
-int matrix_det(double** matr, int n);
 
 
 
